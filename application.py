61e392c85ccbaaa7f8f16a12f91098d3cefc2dfa
root <root@ip-172-31-46-48.ap-south-1.compute.internal>
2024-04-19 12:11:04 +0000

    new changes

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ def insertemployee():
         salary = details['salary']
         my_database.execute("INSERT INTO employee(empno, empname, salary) VALUES (%s, %s, %s)", (empno, empname, salary))
         mysqldb.commit()
-        return 'Employee successfullly created kindly check it'
+        return 'Employee successfullly created kindly check'
     return render_template('insertemployee.html')
 
 @app.route('/listofemployees', methods=['GET'])
