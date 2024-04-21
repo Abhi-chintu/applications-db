ad9df2250ad05262d2d65b9d7acf4185f008c790
Abhi-chintu <94033251+Abhi-chintu@users.noreply.github.com>
2024-04-21 22:40:50 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,6 +1,18 @@
 def build() {
-    echo "This is the build stage"
+   sh 'pip install -r requirements.txt'
 }
 
+def dockerImageBuild(){
+    sh "docker build -t abhichintu/python-image:${BUILD_NUMBER} ."
+}
+
+def dockerImagePush(){
+    withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'password', usernameVariable: 'username')]) {
+            sh """
+            docker login -u ${username} -p ${password}
+            docker push abhichintu/python-image:${BUILD_NUMBER}
+            """
+        }
+}
 return this
 
